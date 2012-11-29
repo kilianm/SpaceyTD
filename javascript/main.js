e7c593a54ed1a5928d751b8e076ed13509c70902
Marijn Speelman <marijn@hyves.nl>
2012-11-29 14:42:33 +0100

    moving test

--- javascript/main.js
+++ javascript/main.js
@@ -1,32 +1,96 @@
-/**
- * A bare bones Sprite and sprite Group example.
- *
- * We move a lot of Ship sprites across the screen with varying speed. The sprites
- * rotate themselves randomly. The sprites bounce back from the bottom of the
- * screen.
- */
+
 
 var playsurface = require('./playsurface');
 var gamejs = require('gamejs');
 
+var Enemy = function(playSurface) {
+    console.log(playSurface);
+    // call superconstructor
+    Enemy.superConstructor.apply(this, arguments);
+
+    this.speed = 80; // pixels per second?
+
+    this.originalImage = gamejs.image.load("images/enemy.png");
+    var dims = this.originalImage.getSize();
+
+    // determine target location
+    this.rotation = 0;
+    this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
+
+    // determine start location
+    this.path = playSurface.path;
+    this.path_index = 1; // start moving
+    this.path_target = this.path[this.path_index];
+
+    this.rect = new gamejs.Rect(this.path[0], dims);
+    return this;
+ };
+ // inherit (actually: set prototype)
+ gamejs.utils.objects.extend(Enemy, gamejs.sprite.Sprite);
+ Enemy.prototype.update = function(msDuration) {
+     if (this.rect.collidePoint(this.path_target)) {
+         console.log(this.rect.collidePoint(this.path_target));
+         //console.log(this.rect.left);
+         this.path_index++;
+     }
+     //this.rect.moveIp(this.speed * (msDuration/1000), 0);
+    // - check if current position collides with target position
+    // - if so, determine if we need to move to X or Y, and change
+    // direction accordingly
+    // - change rotation so to point to correct target coordinates
+
+
+
+//     if (target.x > current.x) {
+//         this.rect.moveIp(this.speed * (msDuration/1000), 0);
+//     }
+//     if (target.x < current.x) {
+//         this.rect.moveIp(-this.speed * (msDuration/1000), 0);
+//     }
+//     if (target.y > current.y) {
+//         this.rect.moveIp(0, this.speed * (msDuration/1000));
+//     }
+//     if (target.y > current.y) {
+//         this.rect.moveIp(0, -this.speed * (msDuration/1000));
+//     }
+
+    // moveIp = move in place
+    this.rect.moveIp(this.speed * (msDuration/1000), 0);
+    if (this.rect.right > 800) {
+       this.speed *= -1;
+       this.image = gamejs.transform.rotate(this.originalImage, this.rotation + 180);
+    } else if (this.rect.right < 0 ) {
+       this.speed *= -1;
+       this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
+    }
+ };
+
+
 function main() {
-   // screen setup
-   gamejs.display.setMode([800, 600]);
-   // game loop
-   var mainSurface = gamejs.display.getSurface();
-   mainSurface.fill("#DFDFDF");
+    // screen setup
+    gamejs.display.setMode([800, 600]);
+    gamejs.display.setCaption("Spacey Tower Defense");
 
-   playSurface = new playsurface.PlaySurface([800, 600], [[0,10], [300,10], [300,100], [600,100], [600,300], [200,300], [200,400], [400,400], [400,600]]);
-   playSurface.draw(mainSurface);
+    // game loop
+    var mainSurface = gamejs.display.getSurface();
+    var playSurface = new playsurface.PlaySurface([800, 600], [[0,10], [300,10], [300,100], [600,100], [600,300], [200,300], [200,400], [400,400], [400,600]]);
 
-   // msDuration = time since last tick() call
-   var tick = function(msDuration) {
-         // update and draw the ships
-   };
-   gamejs.time.fpsCallback(tick, this, 60);
-}
+    var gEnemies= new gamejs.sprite.Group();
+    for (var i=0;i<1;i++) {
+        gEnemies.add(new Enemy(playSurface));
+    }
 
-/**
- * M A I N
- */
+    // msDuration = time since last tick() call
+    var tick = function(msDuration) {
+        mainSurface.fill("#DFDFDF");
+        playSurface.draw(mainSurface);
+
+        gEnemies.update(msDuration);
+        gEnemies.draw(mainSurface);
+    };
+    gamejs.time.fpsCallback(tick, this, 60);
+ }
+
+gamejs.preload(['images/enemy.png']);
+
 gamejs.ready(main);
