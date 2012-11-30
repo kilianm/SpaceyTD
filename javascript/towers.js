72e60653bc47c3237a48b1b7b37a51768fa8d5ce
Marijn Speelman <marijn@hyves.nl>
2012-11-30 10:19:15 +0100

    unix newlines

--- javascript/towers.js
+++ javascript/towers.js
@@ -3,30 +3,36 @@ var draw = require('gamejs/draw');
 var $v = require('gamejs/utils/vectors');
 
 
+var Tower = function(playSurface, location) {
+    Tower.superConstructor.apply(this, arguments);
+
+    this.playSurface = playSurface;
+
+    this.rotation = 0;
+    this.currentTargetEnemy = null;
+
+    var dims = [40, 40];
+    this.rect = new gamejs.Rect(location, dims);
+};
+gamejs.utils.objects.extend(Tower, gamejs.sprite.Sprite);
+
 //LASER TOWER
 var LaserTower = exports.LaserTower = function(playSurface, location) {
-        LaserTower.superConstructor.apply(this, arguments);
-        this.playSurface = playSurface;
+    LaserTower.superConstructor.apply(this, arguments);
 
-        this.originalImage = gamejs.image.load("images/enemy.png");
-        var dims = [40, 40]; //this.originalImage.getSize();
-        // determine target location
-        this.rotation = 0;
-        this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
+    this.originalImage = gamejs.image.load("images/enemy.png");
+    this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
 
-        this.shootRange = 150;
-        this.shootDamage = 3;
-
-        this.currentTargetEnemy = null;
-
-        this.rect = new gamejs.Rect(location, dims);
-        return this;
-    };
-// inherit (actually: set prototype)
-gamejs.utils.objects.extend(LaserTower, gamejs.sprite.Sprite);
-LaserTower.prototype.update = function(msDuration) {
+    // config
     this.rotationSpeed = 50;
+    this.shootRange = 150;
+    this.shootDamage = 3;
 
+    return this;
+};
+// inherit (actually: set prototype)
+gamejs.utils.objects.extend(LaserTower, Tower);
+LaserTower.prototype.update = function(msDuration) {
     var self = this;
     var enemiesInRange = {};
     var enemyDistances = [];
@@ -49,7 +55,7 @@ LaserTower.prototype.update = function(msDuration) {
     } else {
         self.currentTargetEnemy = null;
 
-        // some anymation to turn the turrents ('look for targest') when
+        // some animation to turn the turrents ('look for targets') when
         // turret is idle
         if(self.target_rotation == null) {
             self.target_rotation = Math.random() * 180;
@@ -74,32 +80,28 @@ LaserTower.prototype.draw = function(surface) {
         draw.line(surface, '#FF0000', this.rect.center, this.currentTargetEnemy.rect.center);
     }
     return;
-
 };
 
 
 // BURNING TOWER
 var BurningTower = exports.BurningTower = function(playSurface, location) {
-        BurningTower.superConstructor.apply(this, arguments);
-        this.playSurface = playSurface;
+    BurningTower.superConstructor.apply(this, arguments);
 
-        this.originalImage = gamejs.image.load("images/enemy-1.png");
-        var dims = [40, 40]; //this.originalImage.getSize();
-        // determine target location
-        this.rotation = 0;
-        this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
+    this.originalImage = gamejs.image.load("images/enemy-1.png");
+    this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
 
-        this.shootRange = 80;
-        this.shootDamage = 3;
+    // config
+    this.shootRange = 80;
+    this.shootDamage = 3;
 
-        this.shooting = false;
-        this.shootingAnimation = 0;
+    // dynamic
+    this.shooting = false;
+    this.shootingAnimation = 0;
 
-        this.rect = new gamejs.Rect(location, dims);
-        return this;
-    };
+    return this;
+};
 // inherit (actually: set prototype)
-gamejs.utils.objects.extend(BurningTower, gamejs.sprite.Sprite);
+gamejs.utils.objects.extend(BurningTower, Tower);
 BurningTower.prototype.update = function(msDuration) {
     var self = this;
     var enemiesInRange = [];
@@ -141,31 +143,28 @@ BurningTower.prototype.draw = function(surface) {
 
 //PROJECTILE TOWER
 var ProjectileTower = exports.ProjectileTower = function(playSurface, location) {
-        ProjectileTower.superConstructor.apply(this, arguments);
-        this.playSurface = playSurface;
+    ProjectileTower.superConstructor.apply(this, arguments);
+    this.playSurface = playSurface;
 
-        this.originalImage = gamejs.image.load("images/enemy-1.png");
-        var dims = [40, 40]; //this.originalImage.getSize();
-        // determine target location
-        this.rotation = 0;
-        this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
+    this.originalImage = gamejs.image.load("images/enemy-1.png");
+    this.image = gamejs.transform.rotate(this.originalImage, this.rotation);
 
-        this.shootRange = 150;
-        this.shootDamage = 100;
-        this.projectileSpeed = 150;
-        this.msShootRatio = 500;
+    // config
+    this.rotationSpeed = 50;
+    this.shootRange = 150;
+    this.shootDamage = 100;
+    this.projectileSpeed = 150;
+    this.msShootRatio = 500;
 
-        this.msSinceLastShot = 0;
+    // dynamic
+    this.msSinceLastShot = 0;
 
-        this.rect = new gamejs.Rect(location, dims);
-        return this;
-    };
+    return this;
+};
 // inherit (actually: set prototype)
-gamejs.utils.objects.extend(ProjectileTower, gamejs.sprite.Sprite);
+gamejs.utils.objects.extend(ProjectileTower, Tower);
 ProjectileTower.prototype.update = function(msDuration) {
     this.msSinceLastShot += msDuration;
-
-    this.rotationSpeed = 50;
 
     var self = this;
     var enemiesInRange = {};
@@ -193,7 +192,7 @@ ProjectileTower.prototype.update = function(msDuration) {
     } else {
         self.currentTargetEnemy = null;
 
-        // some anymation to turn the turrents ('look for targest') when
+        // some animation to turn the turrents ('look for targets') when
         // turret is idle
         if(self.target_rotation == null) {
             self.target_rotation = Math.random() * 180;
@@ -217,58 +216,61 @@ ProjectileTower.prototype.draw = function(surface) {
 };
 
 var calculateDegreeByPoints = function(pointLooking, pointLooked) {
-        var dx = pointLooked[0] - pointLooking[0];
-        var dy = pointLooked[1] - pointLooking[1];
-        var theta = Math.atan2(dy, dx);
-        var degrees = theta * 180 / Math.PI; // rads to degs
-        return degrees;
-    };
+    var dx = pointLooked[0] - pointLooking[0];
+    var dy = pointLooked[1] - pointLooking[1];
+    var theta = Math.atan2(dy, dx);
+    var degrees = theta * 180 / Math.PI; // rads to degs
+    return degrees;
+};
 
 var createProjectile = function(playSurface, launchPoint, enemy, speed, damage) {
-        console.log(["launch projectile!", launchPoint, enemy, speed, damage]);
-        var projectile = new Projectile(playSurface, launchPoint, enemy, speed, damage);
-        playSurface.addProjectile(projectile);
-    };
+    console.log(["launch projectile!", launchPoint, enemy, speed, damage]);
+    var projectile = new Projectile(playSurface, launchPoint, enemy, speed, damage);
+    playSurface.addProjectile(projectile);
+};
 
 var Projectile = function(playSurface, location, enemy, speed, damage) {
-        Projectile.superConstructor.apply(this, arguments);
-        this.playSurface = playSurface;
+    Projectile.superConstructor.apply(this, arguments);
+    this.playSurface = playSurface;
 
-        // determine target location
-        this.rotation = 0;
+    // determine target location
+    this.rotation = 0;
 
-        this.speed = speed;
-        this.damage = damage;
+    this.speed = speed;
+    this.damage = damage;
 
-        this.enemy = enemy;
+    this.enemy = enemy;
 
-        this.rect = new gamejs.Rect(location, [1, 1]);
+    this.rect = new gamejs.Rect(location, [1, 1]);
 
-        this.update = function(msDuration) {
+    this.update = function(msDuration) {
 
-            var target = this.enemy.rect.center;
-            var current = this.rect.center;
+        var target = this.enemy.rect.center;
+        var current = this.rect.center;
 
-            var dx = target[0] - current[0];
-            var dy = target[1] - current[1];
+        var dx = target[0] - current[0];
+        var dy = target[1] - current[1];
 
-            var theta = Math.atan2(dy, dx);
+        var theta = Math.atan2(dy, dx);
 
-            var pixel_speed = this.speed * (msDuration / 1000);
+        var pixel_speed = this.speed * (msDuration / 1000);
 
-            var x = Math.cos(theta) * pixel_speed;
-            var y = Math.sin(theta) * pixel_speed;
+        var x = Math.cos(theta) * pixel_speed;
+        var y = Math.sin(theta) * pixel_speed;
 
-            this.rect.moveIp([x,y]);
+        this.rect.moveIp([x,y]);
 
-            //IF hit DAMAGE!! and remove.
-            //also remove when enemy is dead
-        };
-
-        this.draw = function(surface) {
-            draw.circle(surface, 'rgba(255, 255, 0, 0.7)', this.rect.center, 2);
-
-        };
-        return this;
+        // hit!
+        if (dx == 0 && dy == 0) {
+            enemy.doDamage(damage);
+            this.kill();
+        }
     };
+
+    this.draw = function(surface) {
+        draw.circle(surface, 'rgba(255, 255, 0, 0.7)', this.rect.center, 2);
+
+    };
+    return this;
+};
 gamejs.utils.objects.extend(Projectile, gamejs.sprite.Sprite);
